b0ea28db277f9e1ba6ff9efc002a7d08a1370a04
HenryKasai <henrykasai1775@gmail.com>
2020-10-11 14:06:47 -0300

    Exercício resolvido

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 let numerador = prompt("Insira o numerador.");
 let denominador = prompt("Insira o denominador.");
-if (denominador != 0) {
-    alert(`${numerador} dividido por ${denominador} é ${numerador / denominador}`);
-} else{
-    alert("Não é possível dividir por zero");
+if (numerador == null || denominador == null) {
+    alert("Insira um valor válido");
+} else {
+    if (denominador != 0) {
+        alert(`${numerador} dividido por ${denominador} é ${numerador / denominador}`);
+    } else{
+        alert("Não é possível dividir por zero");
+    }
 }
